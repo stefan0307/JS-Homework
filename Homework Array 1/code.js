321bd2ab2bebe2d40911c82ab51310703d37e598
Stefan K <120093408+stefan0307@users.noreply.github.com>
2022-12-19 15:49:57 +0100

    Homework Array 1

---
+++ Homework Array 1/code.js
@@ -0,0 +1,7 @@
+let storyElements = [`Stefan`, `Tired`, `Worked`];
+
+function tellStory (array){
+   console.log(`This is`, array[0], array[0], `is a nice person. Today he is`, array[1], `. He has `, array[2], `all day!`);
+}
+
+tellStory(storyElements);
