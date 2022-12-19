11adb017f3b355808b5cedc15621624922d82336
Stefan K <120093408+stefan0307@users.noreply.github.com>
2022-12-19 15:50:46 +0100

    Homework Array 3

---
+++ Homework Array 3/code.js
@@ -0,0 +1,8 @@
+let array1 = [`Hello`, `there`, `students`, `from`, `SEDC`, `!`, `!`];
+
+function printFullString (array){
+    console.log(`${array[0]} ${array[1]} ${array[2]} ${array[3]} ${array[4]} ${array[5]} ${array[6]}`);
+}
+
+printFullString(array1);
+printFullStringFor(array1);
